488807644926f8beacee707f1d1d631b71f7983c
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-08-23 11:13:38 +0200

    route modification

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,26 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\User as EntityUser;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\InMemoryUser;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
-class LoginController extends AbstractController
+    class LoginController extends AbstractController
 {
     /**
-     * @Route("/login/user", name="app_login", methods="POST")
+     * @Route("/api/v1/login/user", name="app_login_user", methods="POST")
      */
     public function login(
         AuthenticationUtils $authenticationUtils,
         SerializerInterface $serializer,
         Request $request
-    ): Response
+        // InMemoryUser $user
+            ): Response
     {
+
+
+            //  if (null === $user) {
+            //     // if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+
+            //     return $this->json([
+            //         'message' => 'missing credentials',
+            //     ], Response::HTTP_UNAUTHORIZED);
+            // }
+           
+            //         // $token = ...; // somehow create an API token for $user
+           
+            //          return $this->json([
+            //             'message' => 'Welcome to your new controller!',
+            //             'path' => 'src/Controller/ApiLoginController.php',
+            //             'user'  => $user->getUserIdentifier(),
+            //             // 'token' => $token,
+            //           ]);
+
+
+
+
        
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->json([
@@ -32,11 +60,11 @@ class LoginController extends AbstractController
         $userConnected= $this->getUser();
         $jsonUserInfos = $serializer->serialize($userConnected, 'json',['groups' => 'userConnected']);
 
-        $session= $request->getSession();
-        $token = $session->get('token');
+        // $session= $request->getSession();
+        // $token = $session->get('token');
 
-        $bearer = $request->headers->get('Authorization');
-        $accessToken = substr($bearer, 7);
+        // $bearer = $request->headers->get('Authorization');
+        // $accessToken = substr($bearer, 7);
       
        return new JsonResponse($jsonUserInfos,Response::HTTP_OK, [],true );
     }
