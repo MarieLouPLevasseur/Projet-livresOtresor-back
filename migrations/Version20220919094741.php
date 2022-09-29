7c17b3d1266ca36859632666d369fb70d440a2aa
Tiphany-Quemeneur <tiphany-60@hotmail.fr>
2022-09-29 16:51:19 +0200

    Co-authored-by: MarieLouPLevasseur <MarieLouPLevasseur@users.noreply.github.com> Co-authored-by: Aswan1901 <Aswan1901@users.noreply.github.com>

---
+++ migrations/Version20220919094741.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220919094741 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE series (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // $this->addSql('DROP TABLE collection');
+        $this->addSql('ALTER TABLE book_kid ADD series_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE book_kid ADD CONSTRAINT FK_B45EF1EA5278319C FOREIGN KEY (series_id) REFERENCES series (id)');
+        $this->addSql('CREATE INDEX IDX_B45EF1EA5278319C ON book_kid (series_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book_kid DROP FOREIGN KEY FK_B45EF1EA5278319C');
+        // $this->addSql('CREATE TABLE collection (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE series');
+        $this->addSql('DROP INDEX IDX_B45EF1EA5278319C ON book_kid');
+        $this->addSql('ALTER TABLE book_kid DROP series_id');
+    }
+}
